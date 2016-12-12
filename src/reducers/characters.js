15e65657edead2459ba93429af1c786ea712d094
Cotel <miguelcoletomunoz@hotmail.com>
2016-12-12 19:12:04 +0100

    Revert "Added HTTPS images url "
    This reverts commit 686409e4006898ef2831b96eb2195b40f2ee7aa4.

--- src/reducers/characters.js
+++ src/reducers/characters.js
@@ -1,22 +1,22 @@
 export default characters = [
     {
         name: "Tyrion Lannister",
-        image: "https://vignette1.wikia.nocookie.net/hieloyfuego/images/7/79/Tyrion_Lannister_HBO.jpg/revision/latest?cb=20140616100523"
+        image: "http://vignette1.wikia.nocookie.net/hieloyfuego/images/7/79/Tyrion_Lannister_HBO.jpg/revision/latest?cb=20140616100523"
     },
     {
         name: "Cersei Lannister",
-        image: "https://vignette4.wikia.nocookie.net/gameofthrones/images/8/87/Cersei_Lannister_(S05E01).jpg/revision/latest?cb=20150420193418"
+        image: "http://vignette4.wikia.nocookie.net/gameofthrones/images/8/87/Cersei_Lannister_(S05E01).jpg/revision/latest?cb=20150420193418"
     },
     {
         name: "Arya Stark",
-        image: "https://vignette3.wikia.nocookie.net/gameofthrones/images/0/0f/Arya_stark_S5.jpg/revision/latest?cb=20150311174943"
+        image: "http://vignette3.wikia.nocookie.net/gameofthrones/images/0/0f/Arya_stark_S5.jpg/revision/latest?cb=20150311174943"
     },
     {
         name: "Sansa Stark",
-        image: "https://vignette1.wikia.nocookie.net/gameofthrones/images/c/cf/Sansa_Battle_of_Bastards_main.jpg/revision/latest?cb=20160621005428"
+        image: "http://vignette1.wikia.nocookie.net/gameofthrones/images/c/cf/Sansa_Battle_of_Bastards_main.jpg/revision/latest?cb=20160621005428"
     },
     {
         name: "Jon Snow",
-        image: "https://vignette4.wikia.nocookie.net/hieloyfuego/images/8/8e/Game_of_Thrones_5x05.jpg/revision/latest?cb=20150508135940"
+        image: "http://vignette4.wikia.nocookie.net/hieloyfuego/images/8/8e/Game_of_Thrones_5x05.jpg/revision/latest?cb=20150508135940"
     }
 ]
